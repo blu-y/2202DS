8ecfa8c9e8d470e3e8a5985913f29f8995719b93
blu-y <a_o@kakao.com>
2022-10-27 11:44:47 +0900

    HW2

--- HW/PHW2/hw2_2016121150_1.c
+++ HW/PHW2/hw2_2016121150_1.c
@@ -9,69 +9,81 @@
 #define MAX 100
 
 typedef struct _node {
-    int row;
-    int col;
-    int val;
+    /* define node struct */
+    int row, col, val;
     struct _node *next;
 } node;
 
 node* list_init(void){
+    /* initialize list
+       return : list head node
+    */
     node *head = (node*)malloc(sizeof(node));
     node *tail = (node*)malloc(sizeof(node));
     if (head==NULL || tail==NULL) return NULL;
+    // if memory allocation fail, return NULL
     head->next = tail;
     tail->next = tail;
     return head;
 }
 
-void list_append(node* h, int row, int col, int val, int mode){
-    // 리스트에 새로운 값을 추가(오름차순)
-    // 파일 입력 mode = 0, 덧셈 mode = 1 
-    node *s = h;
-    node *p = h->next;
-    node *new = (node*)malloc(sizeof(node));
-    new->row = row;
-    new->col = col;
-    new->val = val;
-    while(p->next != p){
-        // until p = tail
-        if ((p->row < row) || ((p->row == row) && (p->col < col))){
-            s = p;
-            p = p->next;
-        } // if p(r,c) is smaller check next
-        
-        //    break;
-        //} // if value already exists, += val
-        else {
-            break;
-        } // else break
-    }
-    if ((p->col == col) && ((p->row == row))){
-        p->val += val;
-        free(new);
-        if (p->val == 0){
-            s->next = p->next;
-            free(p);
-            if (mode) h->val -= 1;
-        }
-    }
-    else {
-        new->next = p;
-        s->next = new;
-        if (mode) h->val += 1;
-    }
-}
-
 void list_print(node* h){
+    /* print list head to tail*/
     node* n;
     n = h;
-    while(n->next != n){
+    while(n->next != n){ // if not tail
         printf("%d %d %d\n", n->row, n->col, n->val);
         n = n->next;
     }
 }
 
+void list_append(node* h, int row, int col, int val, int mode){
+    /* append new value in list, ascending
+       input : head node, row, column, value, mode
+       mode = 0(file load), 1(add sparse matrix)
+    */ 
+    node *s = h;
+    node *p = h->next;
+    node *new = (node*)malloc(sizeof(node));
+    // order : s > new > p
+    new->row = row;
+    new->col = col;
+    new->val = val;
+    // fill new node infos
+    while(p->next != p){
+        // until p = tail
+        if ((p->row < row) || ((p->row == row) && (p->col < col))){
+            s = p;
+            p = p->next;
+        } // if p's row, column is smaller check next node
+        else {
+            break;
+        } // else break
+    }
+    if (mode && ((p->col == col) && ((p->row == row)))){
+        p->val += val;
+        free(new);
+        // in adding mode, if new row, column is same with p
+        // add values and free new node
+        if (p->val == 0){
+            s->next = p->next;
+            free(p);
+            h->val -= 1;
+        } // if added value is 0, delete node
+    }
+    else {
+        new->next = p;
+        s->next = new;
+        if (mode) h->val += 1;
+    } // insert new node between s and p
+      // if adding mode, num of value(head->val) += 1
+}
+
 node* smat_load(char* str, node* h){
+    /* load sparse matrix file to list
+       input : filename, list head node
+       return : list head node
+    */
     FILE* fp;
     char line[MAX];
     int last = 0;
@@ -80,32 +92,31 @@ node* smat_load(char* str, node* h){
         printf("파일이 없습니다\n");
         return h;
     }
-    /* first line */
+    /* first line -> head node */
     fgets(line, MAX, fp);
-    if (line[strlen(line)-1] != 10) last = 1;
-    // lastline = contents NULL
-    else line[strlen(line)-2] = 0;
-    // not lastline = contents CR LF NULL > contents NULL
-    h->row = atoi(strtok(line, " "));
-    h->col = atoi(strtok(NULL, " "));
-    h->val = atoi(strtok(NULL, " "));
-    /* second to last line */
+    if (line[strlen(line)-1] != 10) last = 1; // lastline == {CONTENTS, NULL}
+    else line[strlen(line)-2] = 0; // not lastline == {CONTENTS,CR,LF,NULL} make {CONTENTS,NULL}
+    h->row = atoi(strtok(line, " ")); // strtok separates string by " "
+    h->col = atoi(strtok(NULL, " ")); // head node contains matrix size
+    h->val = atoi(strtok(NULL, " ")); // and number of non 0 value
+    /* second to last line -> append(sorted) */
     while(!last){
         fgets(line, MAX, fp);
-        if (line[strlen(line)-1] != 10) last = 1;
-        // lastline = contents NULL
-        else line[strlen(line)-2] = 0;
-        // not lastline = contents CR LF NULL > contents NULL
-        row = atoi(strtok(line, " "));
+        if (line[strlen(line)-1] != 10) last = 1; // lastline == {CONTENTS, NULL}
+        else line[strlen(line)-2] = 0; // not lastline == {CONTENTS,CR,LF,NULL} make {CONTENTS,NULL}
+        row = atoi(strtok(line, " ")); // strtok separates string by " "
         col = atoi(strtok(NULL, " "));
         val = atoi(strtok(NULL, " "));
-        list_append(h, row, col, val, 0);
+        list_append(h, row, col, val, 0); // append to list (mode 0)
     }
     fclose(fp);
     return h;
 }
 
 void smat_save(char *str, node* h){
+    /* load sparse matrix file to list
+       input : filename, list head node
+    */
     FILE* fp;
     node* n = h;
     char temp[MAX];
@@ -113,19 +124,22 @@ void smat_save(char *str, node* h){
         printf("파일 저장 실패\n");
         return;
     }
-    /* first to last-1 line*/
+    /* first to last-1 line */
     while(n->next->next != n->next){
+        // make string with row column value and new_line and write to file
         sprintf(temp, "%d %d %d\n", n->row, n->col, n->val);
         fputs(temp, fp);
         n = n->next;
     }
     /* last line */
+    // make string with row column value and write to file
     sprintf(temp, "%d %d %d", n->row, n->col, n->val);
     fputs(temp, fp);
     fclose(fp);
 }
 
 void smat_print(node *h){
+    /* print sparse matrix list with matrix form */
     node *n = h->next;
     int row = h->row;
     int col = h->col;
@@ -133,33 +147,39 @@ void smat_print(node *h){
     while(n->next != n){
         mat[col*n->row + n->col] = n->val;
         n = n->next;
-    }
+    } // make matrix with list
     for (int i = 0; i < row; i++){
         for (int j = 0; j < col; j++) printf("%d ", mat[i*col+j]);
         printf("\n");
-    }
+    } // print matrix[i*col+j] i=0~row-1, j=0~col-1
 }
 
 node* smat_add(node *h1, node *h2){
+    /* add 2 sparse matrix with list form
+       input : list1 head node, list2 head node
+       return : list1 head node(summed)
+    */
     if (!(h1->row == h2->row) && (h1->col == h2->col)){
         printf("행렬의 크기가 다릅니다!\n");
         return h1;
-    }
+    } // if matrix size does not match
     node* n;
     n = h2->next;
     while(n->next != n){
         list_append(h1, n->row, n->col, n->val, 1);
         n = n->next;
-    }
+    } // append head to tail (except head and tail) (add mode)
     return h1;
 }
 
 void input(char* s){
+    /* get string and delete new_line */
     fgets(s, MAX, stdin);
     if (s[strlen(s)-1] == '\n') s[strlen(s)-1] = '\0';
 }
 
 int option(void){
+    /* get integer using input function */
     char s[5];
     input(s);
     return atoi(s);
@@ -204,7 +224,7 @@ int main(void){
             default:
                 break;
         }
-    } while(c);
+    } while(c); // if input is 0 or NULL break
     printf("Bye~\n");
     return 0;
 }
--- HW/PHW2/hw2_2016121150_2.c
+++ HW/PHW2/hw2_2016121150_2.c
@@ -5,7 +5,173 @@
 // 2016121150 윤준영
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 100
 
+typedef struct _node {
+    /* define node struct */
+    int val;
+    struct _node *left;
+    struct _node *right;
+} node;
+
+typedef struct _hnode {
+    node* stack[MAX];
+    int top;
+    struct _node *tree;
+} hnode;
+
+hnode* tree_init(void){
+    /* initialize list
+       return : list head node
+    */
+    hnode *head = (hnode*)malloc(sizeof(hnode));
+    node *tail = (node*)malloc(sizeof(node));
+    if (head==NULL || tail==NULL) return NULL;
+    // if memory allocation fail, return NULL
+    head->top = -1;
+    head->tree = tail;
+    tail->left = tail;
+    tail->right = tail;
+    return head;
+}
+
+void input(char* s){
+    /* get string and delete new_line */
+    fgets(s, MAX, stdin);
+    if (s[strlen(s)-1] == '\n') s[strlen(s)-1] = '\0';
+}
+
+int str2int(void){
+    /* get integer using input function */
+    char s[5];
+    input(s);
+    return atoi(s);
+}
+
+int is_operator(int i){
+    return (i == '+' || i == '-' || i == '*' || i == '/');
+}
+
+int is_num(int i){
+    if (i >= 48 && i <= 57) return i-48;
+    else return i;
+}
+
+node* pop(hnode *h){
+    if (h->top == -1){
+        printf("Stack underflow!\n");
+        return NULL;
+    }
+    node* ret = h->stack[h->top];
+    h->top -= 1;
+}
+
+void push(node *a, hnode *h){
+    if (h->top == 99){
+        printf("Stack overflow!\n");
+        return;
+    }
+    h->top += 1;
+    h->stack[h->top] = a;
+}
+
+void parse_tree(char *s, hnode *h){
+    node *n;
+    node *tail = h->tree;
+    while(*s){
+        while (*s == ' ') s++; // skip ' '
+        n = (node*)malloc(sizeof(node));
+        n->val = *s;
+        n->left = tail;
+        n->right = tail;
+        if (is_operator(*s)){
+            n->right = pop(h);
+            n->left = pop(h);
+        }
+        push(n, h);
+        s++;
+    }
+    h->tree = pop(h);
+}
+
+int operate(node *n, int l, int r){
+    if (n->val == '+') return l + r;
+    if (n->val == '-') return l - r;
+    if (n->val == '*') return l * r;
+    if (n->val == '/') return l / r;
+}
+
+int parse_tree_calc(int x, node *t){
+    /* 1. if t is not leaf (if t->left and t-> right is not same(tail)),
+          then it is operator: calculate chilren first
+       2. if t is a leaf:
+         2-1. if t->val is x, return x
+         2-2. if t->val is number, return number
+    */
+    int ret = 0;
+    int l = 0;
+    int r = 0;
+    // 오류나는 곳
+    if (t->left != t->right){
+        printf("one %d ", ret);
+        l = parse_tree_calc(x, t->left);
+        r = parse_tree_calc(x, t->right);
+        ret = operate(t, l, r);
+        printf("one %d ", ret);
+        return ret;
+    }
+    if (t->val == 'x'){
+        printf("two %d ", x);
+        return x;
+    }
+    else{
+        printf("three %d ", t->val);
+        return t->val;
+    }
+}
+
+void parse_tree_pre2in(hnode *h, char *fx){
+
+
+
+    printf("f(x) = %s\n", fx);
+}
+
+int main(void){
+    int stack[MAX] = {0};
+    hnode *h1 = tree_init();
+    int c;
+    char fx[MAX];
+    char fx2[MAX];
+    int x;
+    int result;
+    do{
+        printf("> 원하는 연산을 선택하세요 (1:f(x)입력, 2:함수값 계산, 3:중위식 출력, 4:종료): ");
+        c = str2int();
+        switch (c){
+            case 1:
+                printf("함수 f(x)를 후위식으로 입력하세요: ");
+                input(fx);
+                parse_tree(fx, h1);
+                break;
+            case 2:
+                printf("x값을 을 입력하세요: ");
+                x = str2int();
+                result = parse_tree_calc(x, h1->tree);
+                printf("f(%d) = %d\n", x, result);
+                break;
+            case 3:
+                parse_tree_pre2in(h1, fx2);
+                break;
+            default:
+                break;
+        }
+    } while(c != 4); // if input is 4
+    printf("Bye~\n");
+    return 0;
+}
 
 /*
 > 원하는 연산을 선택하시오 (1:f(x)입력, 2:함수값 계산, 3:중위식 출력, 4:종료): 1
